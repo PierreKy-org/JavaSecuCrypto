d4c339c86413f6fb803f7b28ecf0176dc2824cf7
Pierre K <41651765+PierreKy-org@users.noreply.github.com>
2023-01-24 01:21:14 +0100

    Add files via upload

--- ClassLoaders/ClassLoading.java
+++ ClassLoaders/ClassLoading.java
@@ -4,26 +4,39 @@ import java.net.*;
 
 
 public class ClassLoading {
-  public static void main(String argv[])
-  {
-    ClassLoading cl=new ClassLoading();
+  
+  public static void main(String argv[]) throws Exception {
+    ClassLoading cl = new ClassLoading();
     cl.haveARide();
   }
 
   String Name_of_Vehicle;
 
-  public void haveARide() {
+  public void haveARide() throws InstantiationException, IllegalAccessException, ClassNotFoundException, MalformedURLException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
     Name_of_Vehicle = "Car";
-    // ...
-
+    Class car = Class.forName(Name_of_Vehicle);
+    Vehicle v = (Vehicle) car.newInstance();
+    v.ride();
+    
     Name_of_Vehicle = "Truck";
-    // ...
+    Class truck = RMIClassLoader.loadClass(Name_of_Vehicle);
+    v = (Vehicle) truck.newInstance();
+    v.ride();
 
     Name_of_Vehicle = "Bicycle";
-    // ...
+    // load class bicycle with rmi remote file 
+    //Pas sûr
+    /* Class bicycle = RMIClassLoader.loadClass("http://localhost:8080/ClassLoaders/Bicycle.java", Name_of_Vehicle);
+    v = (Vehicle) bicycle.newInstance();
+    v.ride(); */
 
     Name_of_Vehicle = "Motorcycle";
-    // ...
+    //Marche pas à voir pourquoi
+    /*MyClassLoader MyClassLoader = new MyClassLoader();
+    Class motorcycle = MyClassLoader.loadClass(Name_of_Vehicle);
+    v = (Vehicle) motorcycle.newInstance();
+    v.ride();*/
+
 
   }
 
